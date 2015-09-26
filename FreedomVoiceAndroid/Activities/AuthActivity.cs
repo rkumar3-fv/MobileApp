9254a87081b3a94b2d3da0db3f18da14ca294081
Sergey <stainrz@gmail.com>
2015-09-26 18:55:18 +0300

    Xamarin.Android project init

---
+++ FreedomVoiceAndroid/Activities/AuthActivity.cs
@@ -0,0 +1,24 @@
+﻿using Android.App;
+using Android.OS;
+
+namespace com.FreedomVoice.MobileApp.Android.Activities
+{
+    /// <summary>
+    /// Authorization activity
+    /// </summary>
+    [Activity(
+        Label = "@string/ApplicationTitle", 
+        MainLauncher = true, 
+        Icon = "@mipmap/ic_launcher", 
+        Theme = "@style/AppThemeActionBar",
+        NoHistory = true)]
+    public class AuthActivity : BaseActivity
+    {
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+            SetContentView(Resource.Layout.act_auth);
+        }
+    }
+}
+
---
+++ FreedomVoiceAndroid/Properties/AssemblyInfo.cs
@@ -0,0 +1,13 @@
+﻿using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("FreedomVoice.MobileApp.Android")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("Debug")]
+[assembly: AssemblyCompany("FreedomVoice")]
+[assembly: AssemblyProduct("FreedomVoice.MobileApp.Android")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+[assembly: AssemblyVersion("0.1.*")]
