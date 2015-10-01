8164fd1b5461bc6ab8608e4744d0a116a08c60e8
Yamalov Azat <yamalov.azat@gmail.com>
2015-10-01 17:48:44 +0300

    *Добавил пару методов

---
+++ FreedomVoice.Core/Entities/PresentationPhoneNumbers.cs
@@ -0,0 +1,10 @@
+﻿namespace FreedomVoice.Core.Entities
+{
+    using Newtonsoft.Json;
+
+    public class PresentationPhoneNumbers
+    {
+        [JsonProperty("PresentationPhoneNumbers")]
+        public string[] PhoneNumbers { get; set; }
+    }
+}
