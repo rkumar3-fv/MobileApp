710465a87b1f782cd5eb9e6bd4c70c4eb8e4b7cc
Mikhail Doronin <mihaild@waveaccess.ru>
2015-10-26 20:52:40 +0300

    save account phonenumber to application settings

---
+++ FreedomVoice.iOS/Helpers/Settings.cs
@@ -0,0 +1,16 @@
+﻿using Foundation;
+using FreedomVoice.Core.Utils;
+
+namespace FreedomVoice.iOS.Helpers
+{
+    public class Settings
+    {
+        const string accountPhoneNumber = "AccountPhoneNumber";        
+
+        public static void SaveAccountPhoneNumber(string userPhoneNumber)
+        {
+            NSUserDefaults.StandardUserDefaults.SetString(DataFormatUtils.ToPhoneNumber(userPhoneNumber), accountPhoneNumber);
+            NSUserDefaults.StandardUserDefaults.Synchronize();
+        }
+    }
+}
