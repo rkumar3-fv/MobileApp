e74ac20dd3364f8343c9d37ccb62cbfb807fdbb9
Mikhail Doronin <mihaild@waveaccess.ru>
2015-11-02 13:31:46 +0300

    callerid view redesign

--- FreedomVoice.iOS/Views/Shared/CallerIdView.cs
+++ FreedomVoice.iOS/Views/Shared/CallerIdView.cs
@@ -28,7 +28,9 @@ namespace FreedomVoice.iOS.Views.Shared
         {
             var dropdownImage = new UIImageView(new CGRect(278, 16, 10, 7)) { Image = UIImage.FromFile("dropdown_arrow.png") };
 
-            var labelField = new UILabel(new CGRect(15, 0, 145, 40)) { Text = "Show as Caller ID:", Font = UIFont.SystemFontOfSize(16f, UIFontWeight.Regular) };
+            var labelField = new UILabel(new CGRect(15, 0, 145, 40)) {
+                Text = "Show as Caller ID:",
+                Font = UIFont.SystemFontOfSize(16f, UIFontWeight.Regular)};
 
             if (_selectedPresentationNumber == null && numbers != null && numbers.Count > 0)
                 _selectedPresentationNumber = numbers[0];
@@ -61,16 +63,22 @@ namespace FreedomVoice.iOS.Views.Shared
             };
             _callerIdTextField.TouchDown += SetPicker;
 
-            var toolbar = new UIToolbar { BarStyle = UIBarStyle.Black, Translucent = true };
+            var toolbar = new UIToolbar { BarStyle = UIBarStyle.Black, Translucent = true, BarTintColor = UIColor.FromRGB(230, 234, 238),  };
             toolbar.SizeToFit();
 
-            var doneButton = new UIBarButtonItem("Done", UIBarButtonItemStyle.Done, (s, e) => {
-                                                                 _callerIdTextField.Text = _selectedPresentationNumber.FormattedPhoneNumber;
-                                                                 _callerIdTextField.ResignFirstResponder();
-                                                             });
+            var actionLbl = new UILabel(new CGRect(100, 15, 140, 15))  { Font = UIFont.SystemFontOfSize(16, UIFontWeight.Medium), Text="Show as Caller ID" };
+            toolbar.AddSubview(actionLbl);
+
+            var doneButton = new UIBarButtonItem("Done", UIBarButtonItemStyle.Done, (s, e) =>
+            {
+                _callerIdTextField.Text = _selectedPresentationNumber.FormattedPhoneNumber;
+                _callerIdTextField.ResignFirstResponder();
+            }){  TintColor = UIColor.FromRGB(3, 138, 193) };
+
             toolbar.SetItems(new[] { doneButton }, true);
 
             _callerIdTextField.InputView = _pickerField;
+            _callerIdTextField.InputView.BackgroundColor = UIColor.White;
             _callerIdTextField.InputAccessoryView = toolbar;
 
             AddSubviews(labelField, _callerIdTextField, dropdownImage);
