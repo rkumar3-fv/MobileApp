57746c83dd6e8a82a5a5c11f251c5ae52f960806
Alexander Pyatakov <Alexander.Pyatakov@waveaccess.ru>
2015-11-25 22:43:56 +0300

    Quick fix for account on hold

--- FreedomVoice.iOS/Services/BaseService.cs
+++ FreedomVoice.iOS/Services/BaseService.cs
@@ -8,7 +8,7 @@ namespace FreedomVoice.iOS.Services
     {
         protected static ErrorResponse CheckErrorResponse<T>(BaseResult<T> baseResult, bool checkOnNullResult = true)
         {
-            if (checkOnNullResult && baseResult.Result == null)
+            if (baseResult.Code == ErrorCodes.Ok && checkOnNullResult && baseResult.Result == null)
                 return new ErrorResponse(ErrorResponse.ErrorInternal);
 
             switch (baseResult.Code)
