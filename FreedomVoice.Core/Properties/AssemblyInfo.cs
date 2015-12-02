be5c09e869005cd3af23bef8b2a9fcd0f2256c96
Sergey <stainrz@gmail.com>
2015-12-02 17:12:39 +0300

    fv test version

--- FreedomVoice.Core/Properties/AssemblyInfo.cs
+++ FreedomVoice.Core/Properties/AssemblyInfo.cs
@@ -14,5 +14,5 @@ using System.Reflection;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: NeutralResourcesLanguage("en")]
-[assembly: AssemblyVersion("0.12.6")]
-[assembly: AssemblyFileVersion("0.12.6")]
+[assembly: AssemblyVersion("0.13.1")]
+[assembly: AssemblyFileVersion("0.13.1")]
