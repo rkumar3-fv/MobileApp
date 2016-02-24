cf33a0d395bb1059afbc5d21914c8cef334c9a0a
Alexander Pyatakov <alexander.pyatakov@waveaccess.ru>
2016-02-24 20:29:49 +0300

    App version changed to 1.0.31

--- FreedomVoice.iOS/Properties/AssemblyInfo.cs
+++ FreedomVoice.iOS/Properties/AssemblyInfo.cs
@@ -33,3 +33,5 @@ using System.Runtime.InteropServices;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.30.0")]
 [assembly: AssemblyFileVersion("1.0.30.0")]
+[assembly: AssemblyVersion("1.0.31.0")]
+[assembly: AssemblyFileVersion("1.0.31.0")]
