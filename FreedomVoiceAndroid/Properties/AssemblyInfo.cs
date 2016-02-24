1175386caaae08a203ef368dcfee748059eea552
Sergey <stainrz@gmail.com>
2016-02-24 15:00:27 +0300

    Version bump

--- FreedomVoiceAndroid/Properties/AssemblyInfo.cs
+++ FreedomVoiceAndroid/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("0.15.29")]
+[assembly: AssemblyVersion("0.15.30")]
