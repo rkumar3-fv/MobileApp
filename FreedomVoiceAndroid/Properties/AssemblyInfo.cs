d869974ed6c79d79d80633c91f54ff460973717e
Sergey <stainrz@gmail.com>
2016-04-07 21:39:36 +0300

    NuGet packages update

--- FreedomVoiceAndroid/Properties/AssemblyInfo.cs
+++ FreedomVoiceAndroid/Properties/AssemblyInfo.cs
@@ -14,4 +14,4 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: ComVisible(false)]
-[assembly: AssemblyVersion("1.0.0.2")]
+[assembly: AssemblyVersion("1.0.0.3")]
