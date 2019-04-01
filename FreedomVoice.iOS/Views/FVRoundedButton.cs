218f090ab731e2ab6161ba7b903d7c9d10f3a128
Eugene Kalyada <mail@color-rain.ru>
2019-04-01 13:19:59 +0500

    fix: (ios) Update lebirarises for iOS project
    Updated build version and.
     Added view file for correct hit testing. iOS missed tap on keyboards.
    META: SML-5636

--- FreedomVoice.iOS/Views/FVRoundedButton.cs
+++ FreedomVoice.iOS/Views/FVRoundedButton.cs
@@ -1,10 +1,20 @@
 ﻿using System;
+using CoreGraphics;
+using MRoundedButton;
+using UIKit;
+
 namespace FreedomVoice.iOS.Views
 {
-    public class FVRoundedButton
+    public class FVRoundedButton: RoundedButton
     {
-        public FVRoundedButton()
+        public FVRoundedButton(CGRect frame, RoundedButtonStyle style, string identifier) : base(frame, style, identifier)
         {
+        }
+
+        public override UIView HitTest(CGPoint point, UIEvent uievent)
+        {
+            var view = base.HitTest(point, uievent);
+            return view;
         }
     }
 }
