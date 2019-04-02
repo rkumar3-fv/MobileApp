318a3ce7c599f2b0c0639a1dd5ce5ba3800aa82d
Eugene Kalyada <mail@color-rain.ru>
2019-04-02 22:40:33 +0500

    feat: (android) New screen for texting
    Conversation list
    META: SML-5648

---
+++ FreedomVoiceAndroid/Fragments/ConversationsFragment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Android.Views;
+using com.FreedomVoice.MobileApp.Android.Helpers;
+
+namespace com.FreedomVoice.MobileApp.Android.Fragments
+{
+    public class ConversationsFragment : BasePagerFragment
+    {
+        public ConversationsFragment()
+        {
+        }
+
+        protected override View InitView()
+        {
+            var view = Inflater.Inflate(Resource.Layout.frag_conversations, null, false);
+            return view;
+        }
+
+        protected override void OnHelperEvent(ActionsHelperEventArgs args)
+        {
+            //throw new NotImplementedException();
+        }
+    }
+}
