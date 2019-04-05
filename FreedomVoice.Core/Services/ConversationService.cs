c98d9f1a08fb8d4764883b86fe4d8b6971dcffe0
Dmitrii Shilov <dshilov@godaddy.com>
2019-04-05 17:53:34 +0500

    SML-5651 Added interface for conversation service
    Signed-off-by: Dmitrii Shilov <dshilov@godaddy.com>

---
+++ FreedomVoice.Core/Services/ConversationService.cs
@@ -0,0 +1,16 @@
+﻿using FreedomVoice.Core.Entities.Texting;
+using FreedomVoice.Core.Services.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreedomVoice.Core.Services
+{
+    public class ConversationService : IConversationService
+    {
+        public ConversationListResponse GetList(int count)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ FreedomVoice.Core/Services/Interfaces/IConversationService.cs
@@ -0,0 +1,12 @@
+﻿using FreedomVoice.Core.Entities.Texting;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FreedomVoice.Core.Services.Interfaces
+{
+    public interface IConversationService
+    {
+        ConversationListResponse GetList(int count);
+    }
+}
