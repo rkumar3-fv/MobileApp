3929c95a3b0e10028878de9d17021ae0c0115142
Eugene Kalyada <evgeniy.kalyada@waveaccess.ru>
2019-04-24 19:03:09 +0500

    fix: (texting) fixed ordering for conversation

--- FreedomVoice.Core/Presenters/ConversationPresenter.cs
+++ FreedomVoice.Core/Presenters/ConversationPresenter.cs
@@ -97,8 +97,7 @@ namespace FreedomVoice.Core.Presenters
             foreach (var row in res.Messages)
             {
                 if (row.From == null) continue;
-                var date = row.From.PhoneNumber.Equals(PhoneNumber) ? row.SentAt : row.CreatedAt;
-                var dateStr = date?.ToString("MM/dd/yyyy");
+                var dateStr = row.CreatedAt?.ToString("MM/dd/yyyy");
                 if (dateStr == null) continue;
                 var pack = _rawData.ContainsKey(dateStr) ? _rawData[dateStr] : new List<IChatMessage>();
                 if (row.From.PhoneNumber.Equals(PhoneNumber))
