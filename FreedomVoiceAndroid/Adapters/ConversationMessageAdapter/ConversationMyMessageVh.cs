3e9d06be0fd9eec7594c5107d4692c351e6cb574
Eugene Kalyada <evgeniy.kalyada@waveaccess.ru>
2019-07-16 16:27:41 +0300

    android api 16 chat screen visual fixes

--- FreedomVoiceAndroid/Adapters/ConversationMessageAdapter/ConversationMyMessageVh.cs
+++ FreedomVoiceAndroid/Adapters/ConversationMessageAdapter/ConversationMyMessageVh.cs
@@ -10,14 +10,14 @@ namespace com.FreedomVoice.MobileApp.Android.Adapters
     class ConversationMyMessageVh : RecyclerView.ViewHolder
     {
         public readonly TextView MessageTv;
-        public readonly LinearLayout ContainerLl;
+        public readonly FrameLayout ContainerLl;
         public readonly TextView Date;
         public readonly ImageView Icon;
 
         public ConversationMyMessageVh(View itemView) : base(itemView)
         {
             MessageTv = itemView.FindViewById<TextView>(Resource.Id.conversation_message_tv);
-            ContainerLl = itemView.FindViewById<LinearLayout>(Resource.Id.conversation_message_fl);
+            ContainerLl = itemView.FindViewById<FrameLayout>(Resource.Id.conversation_message_fl);
             Date = itemView.FindViewById<TextView>(Resource.Id.item_message_date);
             Icon = itemView.FindViewById<ImageView>(Resource.Id.item_my_message_iv);
         }
